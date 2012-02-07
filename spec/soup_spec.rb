cd4f4e397f8120f5c11240c98f865c32b07caff5
Darek Finster <darek.finster@gmail.com>
2012-02-07 03:03:39 +0100

    rspec framework

--- spec/soup_spec.rb
+++ spec/soup_spec.rb
@@ -9,13 +9,8 @@ describe "Soup Agent" do
             builder.use Faraday::Adapter::NetHttp
           end
   end
-<<<<<<< HEAD
 
   it "should connect to soup.io" do
     @agent.host.should == "www.soup.io"
-=======
-  it "should connect to soup" do
-    @agent.host.should == "http://soup.io"
->>>>>>> b5f2493c1134eb2b59c428ffd8b25cb41da3d228
   end
 end
