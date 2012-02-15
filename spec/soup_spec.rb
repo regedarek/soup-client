27d0f5a22735b3c0261f5190db9f28e96a43a1d9
Darek Finster <darek.finster@gmail.com>
2012-02-16 00:46:40 +0100

    log in spec

--- spec/soup_spec.rb
+++ spec/soup_spec.rb
@@ -1,16 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'faraday'
-
-describe "Soup Agent" do
+require "net/http"
+describe "Soup Client" do
   before do
-    @agent = Faraday.new(url: 'http://www.soup.io/') do |builder|
-            builder.use Faraday::Request::UrlEncoded
-            builder.use Faraday::Response::Logger
-            builder.use Faraday::Adapter::NetHttp
-          end
+    @client = Soup::Client.new("soup-client", "soup-client")
   end
 
-  it "should connect to soup.io" do
-    @agent.host.should == "www.soup.io"
+  it "should log in to soup.io" do
+    @client.login
+    get '/login'
+    last_response.status.should == 200
   end
 end
