6ec39a0f2ef218c4e09920beedc93e8e14d1b1ae
Darek Finster <darek.finster@gmail.com>
2012-02-16 01:28:03 +0100

    new link spec

--- spec/soup_spec.rb
+++ spec/soup_spec.rb
@@ -3,10 +3,10 @@ require 'faraday'
 describe "Soup Client" do
   before do
     @client = Soup::Client.new("soup-client", "soup-client")
+    @client.login
   end
 
   it "should ad new link to soup.io" do
-    @client.login
     @client.new_link("http://wp.pl", title = 'wp', description = 'WP')
   end
 end
